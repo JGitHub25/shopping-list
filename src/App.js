95f0db1a281432bcdb7b379e39573912388f1bf2
jpeg25 <jairo25509@hotmail.com>
2022-01-10 16:57:13 -0500

    feat: style.

--- src/App.js
+++ src/App.js
@@ -1,24 +1,35 @@
-import logo from "./logo.svg";
-import "./App.css";
+import { useState } from "react";
 
 function App() {
+  const [task, setTask] = useState("");
+
+  const handleChange = (e) => {
+    setTask(e.target.value);
+  };
+
+  const handleSubmit = (e) => {
+    e.preventDefault();
+  };
+
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          25s <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
+    <main className="main-container">
+      <h2 className="title">grocery bud</h2>
+      <form onSubmit={handleSubmit}>
+        <div className="form-control">
+          <input
+            type="text"
+            className="grocery"
+            placeholder="e.g. broccoli"
+            value={task}
+            onChange={handleChange}
+          />
+          <button type="submit" className="submit-btn">
+            submit
+          </button>
+        </div>
+      </form>
+      <article>list</article>
+    </main>
   );
 }
 
